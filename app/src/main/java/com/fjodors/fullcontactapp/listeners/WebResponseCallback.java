980bb73433a7d208f77256f8b6383ad963aa9fb6
Fjodors Pohodņevs <fjodors@walmoo.com>
2015-08-04 13:30:31 +0300

    changed class into interface

--- app/src/main/java/com/fjodors/fullcontactapp/listeners/WebResponseCallback.java
+++ app/src/main/java/com/fjodors/fullcontactapp/listeners/WebResponseCallback.java
@@ -7,9 +7,10 @@ import org.json.JSONObject;
 /**
  * Created by Fjodors on 2015.05.08..
  */
-public class WebResponseCallback {
-    public void onError(VolleyError data){
-    }
-    public void onData(JSONObject data){
-    }
+public interface WebResponseCallback {
+
+    void onError(VolleyError data);
+
+    void onData(JSONObject data);
 }
+
